01cde3dae0557431db7ef2cfb36faa9be1d89d46
Sable <bandidos_150@mail.ru>
2022-11-13 19:57:27 +0300

    задача 56

---
+++ Task56/Program.cs
@@ -0,0 +1,59 @@
+﻿int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write($"{matrix[i, j]}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int LessSum(int[,] matrix)
+{
+    int a = default;
+    int n = default;
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        if(a == default) 
+        {
+            a = sum;
+            n = i;
+        }
+
+        if(sum < a) a = sum;
+        {
+            a = sum;
+            n = i;
+        }
+        
+    }
+    return n;
+}
+int[,] table = CreateMatrix(3, 4, 1, 17);
+PrintMatrix(table);
+int b = LessSum(table);
+Console.WriteLine(b);
